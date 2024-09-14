b493a21ebf842d7df8c4b645525d85a03124051a
vhugojc <vic_360.2000@hotmail.com>
2024-09-13 22:55:12 -0600

    feature/ add new Task form

---
+++ frontend/src/components/NewTaskModal.jsx
@@ -0,0 +1,29 @@
+import { Modal, Form } from 'antd';
+import NewTaskForm from './NewTaskForm';
+
+const NewTaskModal = ({ visible, onCreate, setIsModalVisible }) => {
+  const [form] = Form.useForm();
+
+  return (
+    <Modal
+      title="Add New Task"
+      visible={visible}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch(info => {
+            console.log('Validate Failed:', info);
+          });
+      }}
+      onCancel={e=>setIsModalVisible(false)}
+    >
+      <NewTaskForm form={form} />
+    </Modal>
+  );
+};
+
+export default NewTaskModal;
--- frontend/src/components/TaskSection.jsx
+++ frontend/src/components/TaskSection.jsx
@@ -1,15 +1,26 @@
-import { Card, Col, Row, Pagination } from "antd";
+import { Card, Col, Row, Pagination, Button } from "antd";
 import TaskTable from "./TaskTable";
 import TaskFilter from "./TaskFilter";
-import "./TaskSection.scss"
+import "./TaskSection.scss";
 import { useState } from "react";
+import NewTaskModal from "./NewTaskModal";
 
 const TaskSection = () => {
   const tasksData = [
     { id: 1, name: "Task 1", status: "Completed", dueDate: "2024-03-10" },
     { id: 2, name: "Task 2", status: "In Progress", dueDate: "2024-03-15" },
-    { id: 3, name: "Finish Project", status: "In Progress", dueDate: "2024-03-18" },
-    { id: 4, name: "Meeting with John", status: "Completed", dueDate: "2024-03-20" },
+    {
+      id: 3,
+      name: "Finish Project",
+      status: "In Progress",
+      dueDate: "2024-03-18",
+    },
+    {
+      id: 4,
+      name: "Meeting with John",
+      status: "Completed",
+      dueDate: "2024-03-20",
+    },
     { id: 5, name: "Task 5", status: "Completed", dueDate: "2024-03-22" },
     { id: 6, name: "Task 6", status: "In Progress", dueDate: "2024-03-25" },
     { id: 7, name: "Task 7", status: "Completed", dueDate: "2024-03-28" },
@@ -18,9 +29,10 @@ const TaskSection = () => {
     { id: 10, name: "Task 10", status: "In Progress", dueDate: "2024-04-05" },
     // ... more tasks
   ];
-  
+
   const [tasks, setTasks] = useState(tasksData);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const pageSize = 10; // Number of tasks per page
 
   const handleFilterChange = (filters) => {
@@ -36,13 +48,42 @@ const TaskSection = () => {
     setTasks(filteredTasks);
   };
 
+  const handleAddTask = () => {
+    setIsModalVisible(true);
+  };
+
   const handlePageChange = (page) => {
     setCurrentPage(page);
+  };
+
+  const handleCreate = (values) => {
+    const newTask = {
+      id: tasks.length + 1,
+      ...values,
+    };
+    setTasks([...tasks, newTask]);
+    setIsModalVisible(false);
   };
 
   return (
     <Card className="task-container">
       <div className="task-manager-container">
+        <div
+          className="button-section"
+          style={{
+            textAlign: "right",
+            marginRight: "20px",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <Button
+            type="primary"
+            shape="circle"
+            icon="+"
+            onClick={handleAddTask}
+          />
+        </div>
         <Row gutter={16} className="filter-section">
           <Col xs={24} sm={24} md={12} lg={8} xl={6}>
             <TaskFilter onFilterChange={handleFilterChange} />
@@ -59,6 +100,11 @@ const TaskSection = () => {
             onChange={handlePageChange}
           />
         </div>
+        <NewTaskModal
+        setIsModalVisible={setIsModalVisible}
+          visible={isModalVisible}
+          onCreate={handleCreate}
+        />
       </div>
     </Card>
   );
